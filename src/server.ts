d50ddf52f43181bba644b5cdbada93fa7604fbb8
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-24 08:21:50 -0300

    Midllewares express.json and routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata'
 import './database'
 import express from 'express'
+import router from './router'
 
 const app = express()
+
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.info('*** Server is running ***'))
