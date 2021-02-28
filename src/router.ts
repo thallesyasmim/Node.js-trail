7ae13ea850b530185a4911f563647a3da8d73a73
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-28 12:17:28 -0300

    Route POST surveys

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
+import { SurveyController } from './controllers/SurveyController'
 import { UserController } from './controllers/UserController'
 
 const router = Router()
 
-const userController = new UserController
+const userController = new UserController()
+const surveyController = new SurveyController()
 
 // users
 router.post('/users', userController.create)
+
+// Surveys
+router.post('/surveys', surveyController.create)
 
 export { router }
