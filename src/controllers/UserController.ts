567b46ccd94768d3b55dc43e68b156cbd4956208
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-28 12:17:06 -0300

    Return status 201

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -24,7 +24,7 @@ class UserController {
 
             await userRepository.save(user)
 
-            return response.json(user)
+            return response.status(201).json(user)
 
 
         } catch (error) {
