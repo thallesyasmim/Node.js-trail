8406c718c83bd08effc8556d2c92c5573c2affd4
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-28 21:53:07 -0300

    Route GET answers

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import { SurveyController } from './controllers/SurveyController'
 import { UserController } from './controllers/UserController'
 import { SendMailController } from './controllers/SendMailController'
+import { AnswerController } from './controllers/AnswerController'
 
 const router = Router()
 
 const userController = new UserController()
 const surveyController = new SurveyController()
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
 
 // users
 router.post('/users', userController.create)
@@ -18,5 +20,8 @@ router.get('/surveys', surveyController.index)
 
 // Send Mail
 router.post('/sendMail', sendMailController.execute)
+
+// Answers
+router.get('/answers', answerController.execute)
 
 export { router }
